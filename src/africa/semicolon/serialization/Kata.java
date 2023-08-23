710b7ed32cb393379c19e157023ce9d34b539d95
Olawalesegun <olawalesegun0@gmail.com>
2023-08-23 23:04:10 +0100

    serializer at work

--- src/africa/semicolon/serialization/Kata.java
+++ src/africa/semicolon/serialization/Kata.java
@@ -1,2 +1,17 @@
-package africa.semicolon.serialization;public class Kata {
+package africa.semicolon.serialization;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class Kata {
+
+    public static void main(String[] args) {
+        try(PrintStream printStream = new PrintStream("/home/user/Documents/jToJson/src/africa/semicolon/serialization/output.txt")){
+            System.setOut(printStream);
+            System.out.println("Writing to a file");
+            System.out.println(printStream);
+        }catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
