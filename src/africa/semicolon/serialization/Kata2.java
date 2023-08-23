fd1ad0472ace349100f064e4a48335d7f1262db6
Olawalesegun <olawalesegun0@gmail.com>
2023-08-23 23:03:00 +0100

    serializer at work

--- src/africa/semicolon/serialization/Kata2.java
+++ src/africa/semicolon/serialization/Kata2.java
@@ -1,2 +1,30 @@
-package africa.semicolon.serialization;public class Kata2 {
+package africa.semicolon.serialization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Kata2 {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Car car = new Car();
+        car.setBrand("Toyota");
+        car.setColor("Purple");
+        car.setMileage(3_000_000L);
+        car.setModel("Camry");
+        String location = "/home/user/Documents/jToJson/src/africa/semicolon/serialization/cars.json";
+        Path path = Path.of(location);
+        try(OutputStream outputStream = Files.newOutputStream(path);
+            PrintStream printStream = new PrintStream(outputStream)) {
+            System.setOut(printStream);
+            objectMapper.writeValue(System.out, car);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+    }
 }
