8d024e0765ef0c60b2d3f9694fd70bed8a4fde54
Olawalesegun <olawalesegun0@gmail.com>
2023-08-23 23:05:39 +0100

    serializer and deserializer at work

--- src/africa/semicolon/deserialization/Kata.java
+++ src/africa/semicolon/deserialization/Kata.java
@@ -1,2 +1,27 @@
-package africa.semicolon.deserialization;public class Kata {
+package africa.semicolon.deserialization;
+
+import africa.semicolon.serialization.Car;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class Kata {
+
+    public static void main(String[] args) {
+        String json = "{\"brand\":\"Peugeot\",\"model\":\"404\",\"color\":\"Black\",\"mileage\":4000}";
+        //THe object mapper i a class that is used to convert java object to json and json object to java
+        ObjectMapper mapper = new ObjectMapper();
+        Car car = null;
+        try {
+            car = mapper.readValue(json.getBytes(), Car.class);
+            System.out.println(car.getBrand());
+            System.out.println(car.getColor());
+            System.out.println(car.getModel());
+            System.out.println(car.getMileage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        //Car car = mapper.convertValue(json, Car.class);
+        /*System.out.println(car);*/
+    }
 }
